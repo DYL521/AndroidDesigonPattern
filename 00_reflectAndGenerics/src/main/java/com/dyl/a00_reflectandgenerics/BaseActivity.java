9386ea010022c177d033d71f4436c539589ef951
DYL521 <1016068291@qq.com>
2020-10-02 17:01:04 +0800

    ...

--- 00_reflectAndGenerics/src/main/java/com/dyl/a00_reflectandgenerics/BaseActivity.java
+++ 00_reflectAndGenerics/src/main/java/com/dyl/a00_reflectandgenerics/BaseActivity.java
@@ -8,6 +8,7 @@ public class BaseActivity extends AppCompatActivity {
 
 
     // 上限
+    //      表示传递参数必须是BaseActivity 的子类，包括BaseActivity本身
     public void startActivity(Class<? extends BaseActivity> clazz) {
         Intent intent = new Intent(this, clazz);
         startActivity(intent);
@@ -15,6 +16,7 @@ public class BaseActivity extends AppCompatActivity {
     }
 
     // 下限： 包括MainActivity和它的父类
+    //       表示传递的参数，必须是x的超类（包括BaseActivity 本身）
     public void startActivityDown(Class<? super BaseActivity> clazz) {
         Intent intent = new Intent(this, clazz);
         startActivity(intent);
---
+++ 00_reflectAndGenerics/src/main/java/com/dyl/a00_reflectandgenerics/NormGenericMethod.java
@@ -0,0 +1,6 @@
+package com.dyl.a00_reflectandgenerics;
+
+// 3、 范型方法
+public class NormGenericMethod {
+
+}
